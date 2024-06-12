9e6f6d44b140ec052d75bfefe462e2f116b18701
Gerry MNunez <gerardo.mnunez@gmail.com>
2024-06-11 19:32:49 -0600

    Start pwa with nextjs

---
+++ next.config.js
@@ -0,0 +1,15 @@
+const withPWA = require("@ducanh2912/next-pwa").default({
+    dest: "public",
+    cacheOnFrontEndNav: true,
+    aggressiveFrontEndNavCaching: true,
+    reloadOnOnline: true,
+    swcMinify: true,
+    disable: false,
+    workboxOptions: {
+        disableDevLogs: true,
+    }
+  });
+  
+  module.exports = withPWA({
+    // Your Next.js config
+  });
